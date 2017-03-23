28fd89e815723b12e95672563aff006613990ad0
aysonsteven <aysonsteven@gmail.com>
2017-03-23 14:32:11 +0800

    work on branched[delete,edit]

--- model/base.ts
+++ model/base.ts
@@ -12,27 +12,27 @@ export class Base extends Api {
 
 
     /**
-     * 
-     * 
-     * 
-     * @param req 
-     * 
+     *
+     *
+     *
+     * @param req
+     *
      * @code example code.
         this.config.list( {} ).subscribe( res => {
             console.log(res);
         }, err => {
             console.log(err);
         });
-     * 
+     *
      * @endcode
-     * 
+     *
      * @code
         this.config.list( {page: 2} ).subscribe( res => { } ); // get items of page no 2 of post_config
         this.user.list( { page: 2, limit: 3 } ).subscribe( res => { }); // get 2nd page of users. A pages has 3 users.
         this.config.list( { page: 1, limit: 3, where: 'id LIKE ?', bind: 'my%' } ).subscribe( res => { } ); // get upto 3 post_configs whose id begins with 'my'
         this.config.list( { limit: 1, where: 'id LIKE ?', bind: 'my%', order: 'idx DESC' } ).subscribe( res => {} ); // get the newly created post_config whose id begins with 'my'. only one data will be returned.
      * @endcode
-     * 
+     *
      */
     list( req: LIST = <LIST> {} ) : Observable< any > {
         req['route'] = this.taxonomy + '.list';
@@ -49,11 +49,24 @@ export class Base extends Api {
         req.session_id = this.getSessionId();
         return this.post( req );
     }
-    
+
 
     create( req = {} ) : Observable<any> {
         req['route'] = this.taxonomy + '.create';
         req['session_id'] = this.getSessionId();
         return this.post( req );
+    }
+
+    delete( req = {} ) : Observable<any> {
+      req['route'] = this.taxonomy + '.delete';
+      req['session_id'] = this.getSessionId();
+      return this.post( req );
+    }
+
+    edit( req ={} ) : Observable<any> {
+      req['route'] = this.taxonomy + '.delete';
+      req['session_id'] = this.getSessionId();
+      return this.post( req );
+
     }
 }
