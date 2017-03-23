3c7bc51592f352974ec7a419403d48753cdfa9c2
lancelynyrd <pinedaclp@gmail.com>
2017-03-23 22:12:56 +0800

    adding input to info box

--- pages/components/info-box/info-box.ts
+++ pages/components/info-box/info-box.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 @Component({
   selector: 'info-box',
   templateUrl: './info-box.html',
   styleUrls:['info-box.scss']
 })
 export class InfoBoxComponent {
+
+
+  @Input () infoBoxClass:string  = null;
+  @Input () iconclass:string  = null;
+  @Input () iClass:string = 'fa-gear fa-3x';
+  @Input () textClass:string  = null;
+  @Input () titleClass:string  = null;
+  @Input () contentClass:string  = null;
+
+
+  @Input () titleInnerHTML:string  = 'Title';
+  @Input () contentInnerHTML:string  = 'Content';
+
+
   constructor( ){
 
   }
