1d7b164a81061aba90f44757cdb5399e984f033a
lancelynyrd <pinedaclp@gmail.com>
2017-03-27 23:15:05 +0800

    working with forum

--- pages/admin/forum/forum.ts
+++ pages/admin/forum/forum.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { AdminService } from './../services/admin.service';
-import { PostData, POST, POSTS,
-    LIST, POST_LIST_RESPONSE
- } from './../../../angular-backend';
+import {  PostConfig,PostData,
+  POST, POSTS,
+  LIST, POST_LIST_RESPONSE,
+  CONFIG, CONFIGS, CONFIG_CREATE, CONFIG_CREATE_RESPONSE
+} from './../../../angular-backend';
 @Component({
   selector: 'backend-admin-forum-page',
   templateUrl: './forum.html',
@@ -10,31 +12,75 @@ import { PostData, POST, POSTS,
 })
 export class BackendAdminForumPage {
 
-    posts: POSTS = [];
-    constructor( public admin: AdminService, private postData: PostData ) {
+  posts: POSTS = [];
+  postConfigs: CONFIGS = [];
+  configCreate: CONFIG_CREATE = {};
 
-        admin.onClickMenuMore();
+  constructor( public admin: AdminService,
+               private postData: PostData,
+               private postConfig: PostConfig
+  ) {
 
-
-        let list: LIST = {
-            select: 'idx, title, created',
-            order: 'idx DESC',
-            extra: { file: false, meta: true, post_config_id: 'qna' }
-        };
-        this.postData.list( list ).subscribe( (res: POST_LIST_RESPONSE ) => {
-
-            console.log(res);
-
-            this.posts = res.data.posts;
+    admin.onClickMenuMore();
 
 
-            this.posts.map( (post: POST) => {
-                post.created = ( new Date( parseInt(post.created) * 1000 ) ).toString();
-            });
+    let list: LIST = {
+      //select: 'idx, title, created',
+      order: 'idx DESC',
+      //extra: { file: false, meta: true, post_config_id: 'qna' }
+    };
+    this.loadPostData(list);
+    this.loadPostConfig(list);
+  }
 
 
-        }, err => this.postData.alert( err ));
+  loadPostData( list?:LIST) {
+
+    this.postData.list( list ).subscribe( (res: POST_LIST_RESPONSE ) => {
+
+      console.log(res);
+
+      this.posts = res.data.posts;
 
 
-    }
+      this.posts.map( (post: POST) => {
+        post.created = ( new Date( parseInt(post.created) * 1000 ) ).toString();
+      });
+
+
+    }, err => this.postData.alert( err ));
+
+  }
+
+
+
+  onClickCreateForum() {
+    this.postConfig.create(this.configCreate).subscribe( (res:CONFIG_CREATE_RESPONSE ) => {
+      console.log(res);
+    }, err => this.postConfig.alert(err));
+  }
+
+  loadPostConfig( list?:LIST ) {
+
+    this.postConfig.list( list ).subscribe( (res: POST_LIST_RESPONSE ) => {
+
+      console.log(res);
+
+      this.postConfigs = res.data.configs;
+
+
+      this.postConfigs.map( (config: CONFIG) => {
+        config.created = ( new Date( parseInt(config.created) * 1000 ) ).toString();
+      });
+
+
+    }, err => this.postData.alert( err ));
+  }
+
+  onClickEdit() {
+
+
+  }
+
+
 }
